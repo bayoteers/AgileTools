f3afd839487ef0ef7bbbe3fba76526380dc623e4
Pami Ketolainen <pami.ketolainen@gmail.com>
2012-04-13 13:52:55 +0300

    Initial commit

---
+++ Extension.pm
@@ -0,0 +1,38 @@
+# -*- Mode: perl; indent-tabs-mode: nil -*-
+#
+# The contents of this file are subject to the Mozilla Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# The Original Code is the AgileTools Bugzilla Extension.
+#
+# The Initial Developer of the Original Code is Pami Ketolainen
+# Portions created by the Initial Developer are Copyright (C) 2012 the
+# Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Pami Ketolainen <pami.ketolainen@gmail.com>
+
+package Bugzilla::Extension::AgileTools;
+use strict;
+use base qw(Bugzilla::Extension);
+
+# This code for this is in ./extensions/AgileTools/lib/Util.pm
+use Bugzilla::Extension::AgileTools::Util;
+
+our $VERSION = '0.01';
+
+# See the documentation of Bugzilla::Hook ("perldoc Bugzilla::Hook" 
+# in the bugzilla directory) for a list of all available hooks.
+sub install_update_db {
+    my ($self, $args) = @_;
+
+}
+
+__PACKAGE__->NAME;
---
+++ lib/Util.pm
@@ -0,0 +1,34 @@
+# -*- Mode: perl; indent-tabs-mode: nil -*-
+#
+# The contents of this file are subject to the Mozilla Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# The Original Code is the AgileTools Bugzilla Extension.
+#
+# The Initial Developer of the Original Code is Pami Ketolainen
+# Portions created by the Initial Developer are Copyright (C) 2012 the
+# Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Pami Ketolainen <pami.ketolainen@gmail.com>
+
+package Bugzilla::Extension::AgileTools::Util;
+use strict;
+use base qw(Exporter);
+our @EXPORT = qw(
+    
+);
+
+# This file can be loaded by your extension via 
+# "use Bugzilla::Extension::AgileTools::Util". You can put functions
+# used by your extension in here. (Make sure you also list them in
+# @EXPORT.)
+
+1;
