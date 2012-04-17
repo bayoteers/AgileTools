d5762d4f73c322cdeace0665e4998bb70b7a39db
Pami Ketolainen <pami.ketolainen@gmail.com>
2012-04-17 19:55:24 +0300

    Added Team database schemas

--- Extension.pm
+++ Extension.pm
@@ -35,4 +35,103 @@ sub install_update_db {
 
 }
 
+sub db_schema_abstract_schema {
+    my ($self, $args) = @_;
+    my $schema = $args->{schema};
+
+    # Team information
+    $schema->{agile_teams} = {
+        FIELDS => [
+            id => {
+                TYPE => 'MEDIUMSERIAL',
+                NOTNULL => 1,
+                PRIMARYKEY => 1,
+            },
+            name => {
+                TYPE => 'TINYTEXT',
+                NOTNULL => 1,
+            },
+            group_id => {
+                TYPE => 'INT3',
+                REFERENCES => {
+                    TABLE => 'groups',
+                    COLUMN => 'id',
+                    DELETE => 'SET_NULL',
+                },
+            },
+            process_id => {
+                TYPE => 'INT1',
+                NOTNULL => 1,
+                DEFAULT => 0,
+            },
+        ],
+    };
+
+    # User role definitions
+    $schema->{agile_roles} = {
+        FIELDS => [
+            id => {
+                TYPE => 'SMALLSERIAL',
+                NOTNULL => 1,
+                PRIMARYKEY => 1,
+            },
+            name => {
+                TYPE => 'TINYTEXT',
+                NOTNULL => 1,
+            },
+            custom => {
+                TYPE => 'BOOLEAN',
+                NOTNULL => 1,
+                DEFAULT => 'TRUE',
+            },
+            can_edit_team => {
+                TYPE => 'BOOLEAN',
+                NOTNULL => 1,
+                DEFAULT => 'FALSE',
+            }
+        ],
+        INDEXES => [
+            'agile_roles_name_idx' => ['name'],
+        ],
+    };
+
+    # Team - user - role mapping
+    $schema->{agile_user_role} = {
+        FIELDS => [
+            team_id => {
+                TYPE => 'INT3',
+                NOTNULL => 1,
+                REFERENCES => {
+                    TABLE => 'agile_teams',
+                    COLUMN => 'id',
+                    DELETE => 'CASCADE',
+                },
+            },
+            user_id => {
+                TYPE => 'INT3',
+                REFERENCES => {
+                    TABLE => 'profiles',
+                    COLUMN => 'userid',
+                    DELETE => 'CASCADE',
+                },
+            },
+            role_id => {
+                TYPE => 'INT3',
+                REFERENCES => {
+                    TABLE => 'agile_roles',
+                    COLUMN => 'id',
+                    DELETE => 'CASCADE',
+                },
+            },
+        ],
+        INDEXES => [
+            agile_user_role_unique_idx => {
+                FIELDS => [qw(team_id user_id role_id)],
+                TYPE   => 'UNIQUE',
+            },
+            agile_user_role_user_idx => ['user_id'],
+        ],
+    };
+}
+
 __PACKAGE__->NAME;
