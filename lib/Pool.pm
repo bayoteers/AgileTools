962f0a4795f5541f179f26c5b1894b807e2d828a
Pami Ketolainen <pami.ketolainen@gmail.com>
2012-05-08 01:05:29 +0300

    Finalized Pool.insert_bug() method

--- lib/Pool.pm
+++ lib/Pool.pm
@@ -82,22 +82,28 @@ sub bugs {
     my $self = shift;
     my $dbh = Bugzilla->dbh;
 
-    my $bug_ids = dbh->selectcol_arrayref(
-        "SELECT bug_id FROM bug_agile_pool
-          WHERE pool_id = ? ORDER BY pool_order",
-          undef, $self->id);
-    return Bugzilla::Bug->new_from_list($bug_ids);
+    if (!defined $self->{bugs}) {
+        my $bug_ids = dbh->selectcol_arrayref(
+            "SELECT bug_id FROM bug_agile_pool
+              WHERE pool_id = ? ORDER BY pool_order",
+              undef, $self->id);
+        $self->{bugs} = Bugzilla::Bug->new_from_list($bug_ids);
+    }
+    return $self->{bugs};
 }
 
-=item C<add_bug($bug_id, $order)>
+=item C<insert_bug($bug_id, $order)>
 
-    Description: Adds new bug into the pool
+    Description: Inserts new bug into the pool
     Params:      $bug_id - Bug ID
-                 $order - (optional) Order of the new bug in this pool
+                 $order - (optional) Order of the new bug in this pool,
+                          goes last if not given
+    Note:        Bug can be only in one pool at the time and it will be removed
+                 from any previous pool.
 
 =cut
 
-sub add_bug {
+sub insert_bug {
     my $self = shift;
     my ($bug_id, $order) = @_;
 
@@ -108,32 +114,59 @@ sub add_bug {
 
     my $dbh = Bugzilla->dbh;
     $dbh->bz_start_transaction();
+
+    # Get max order in this pool
     my $max = $dbh->selectcol_arrayref(
-        "SELECT COALESCE(MAX(pool_order), 0)
+        "SELECT COUNT(*)
            FROM bug_agile_pool
-          WHERE bug_id = ? AND pool_id = ?",
-          undef, ($bug_id, $self->id))->[0];
-    if (! defined $order || $order > $max) {
-        $order = $max + 1;
-    }
-    my $existing = $dbh->selectcol_arrayref(
-        "SELECT pool_order
+          WHERE pool_id = ?",
+          undef, $self->id)->[0];
+
+    my ($old_pool, $old_order) = $dbh->selectrow_array(
+        "SELECT pool_id, pool_order
            FROM bug_agile_pool
-          WHERE bug_id = ? AND pool_id = ?",
-          undef, ($bug_id, $self->id))->[0];
+          WHERE bug_id = ?",
+          undef, $bug_id);
 
-    if (defined $existing) {
-        
-    } else {
+    # If the bug is being moved inside this pool, the maximum order is the
+    # number of bugs in the pool, otherwise + 1 (the bug being added)
+    $max = ($old_pool == $self->id) ? $max : $max + 1;
 
+    $order = $max unless (defined $order && $order < $max)
+
+    my $changed = ($old_pool != $self->id || $old_order != $order);
+
+    if (defined $old_pool && $changed) {
+        # Delete old entry
+        dbh->do("DELETE FROM bug_agile_pool
+                    WHERE pool_id = ? AND bug_id = ?",
+            undef, ($old_pool, $bug_id));
+        # Shift bugs in old pool
+        dbh->do("UPDATE bug_agile_pool
+                    SET pool_order = pool_order - 1
+                  WHERE pool_id = ? AND pool_order > ?",
+            undef, ($old_pool, $old_order));
     }
-    dbh->do(
-        "UPDATE bug_agile_pool
-            SET order
-          WHERE pool_id = ? ORDER BY pool_order",
-          undef, $self->id);
-
+    # Note: If the bug is moved inside this pool, the other bugs will probably
+    # get shifted back and forth, but the performance gain from more detailed
+    # queries is probably not worth the introduced complexity...
+    if ($changed) {
+        # Shift bugs in this pool
+        dbh->do("UPDATE bug_agile_pool
+                    SET pool_order = pool_order + 1
+                  WHERE pool_id = ? AND pool_order >= ?",
+            undef, ($self->id, $order));
+        # Insert new entry
+        dbh->do("INSERT INTO bug_agile_pool (bug_id, pool_id, pool_order)
+                      VALUES (?, ?, ?)",
+            undef, ($bug_id, $self->id, $order));
+        delete $self->{bugs};
+    }
+    $dbh->bz_commit_transaction();
+    return $self->bugs;
 }
+
+#TODO: Add pool and pool_order methods to Bug class
 
 =back
 
