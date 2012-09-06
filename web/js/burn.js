6be99e09aee04561ec8901ee0a2ae5497813142d
Pami Ketolainen <pami.ketolainen@jollamobile.com>
2012-09-06 14:41:40 +0300

    Show weekends in burndown chart

--- web/js/burn.js
+++ web/js/burn.js
@@ -14,7 +14,7 @@ var changeDates = function()
     window.location.href = window.location.pathname + query;
 };
 
-var initBurn = function()
+function initBurn()
 {
     var dateOptions = {
 
@@ -56,6 +56,9 @@ var initBurn = function()
             min: BURN.start,
             max: BURN.end,
         },
+        yaxis: {
+            min: 0,
+        },
         grid: {
             markings: getBurnMarkers(),
         },
@@ -64,15 +67,12 @@ var initBurn = function()
     plotBurn(BURN.type);
 };
 
-var plotBurn = function(type) {
+function plotBurn(type)
+{
     var series;
     if (type == 'items') {
         series = [
-            {
-                data: [[BURN.start, BURN.start_open], [BURN.end, 0]],
-                lines: {fill: true, lineWidth: 0},
-                points: {show: false},
-            },
+            getIdealBurn(BURN.start_open),
             {
                 label: "Open items",
                 data: BURN.open_items,
@@ -81,11 +81,7 @@ var plotBurn = function(type) {
         ];
     } else {
         series = [
-            {
-                data: [[BURN.start, BURN.start_rem], [BURN.end, 0]],
-                lines: {fill: true, lineWidth: 0},
-                points: {show: false},
-            },
+            getIdealBurn(BURN.start_rem),
             {
                 label: "Remaining",
                 data: BURN.remaining,
@@ -99,12 +95,74 @@ var plotBurn = function(type) {
     $.plot("#burnchart", series, BURN.chartOptions);
 };
 
-var getBurnMarkers = function() {
-    // TODO
+function getBurnMarkers()
+{
     var now = new Date();
     now = now.getTime();
-    return [
-        // Today markerd
-        { color: 'red', xaxis: {from: now, to: now} },
-    ];
+    var markings = [];
+    var day = new Date(BURN.start);
+    var end = new Date(BURN.end);
+    var weekend = false;
+    var from = 0;
+    while (day < end) {
+        if (day.getDay() != 0 && day.getDay() != 6) {
+            if (weekend) {
+                markings.push({
+                    color:'lightgray',
+                    xaxis: {from: from, to: day.getTime()},
+                });
+                weekend = false;
+            }
+        } else {
+            if(!weekend) {
+                from = day.getTime();
+                weekend = true;
+            }
+        }
+        day.setDate(day.getDate() + 1);
+    }
+    if (weekend) {
+        markings.push({
+            color:'lightgray',
+            xaxis: {from: from, to: end.getTime()},
+        });
+
+    }
+    // Today marker
+    markings.push({ color: 'red', xaxis: {from: now, to: now} });
+
+    return markings;
 };
+
+function getIdealBurn(start_value)
+{
+    var end = new Date(BURN.end);
+    var day = new Date(BURN.start);
+    var delta_days = Math.ceil((end - day) / 1000 / 60 / 60 / 24);
+    var data = [];
+    var week_days = 0;
+    while (day < end) {
+        if (day.getDay() != 0 && day.getDay() != 6) {
+            week_days++;
+        }
+        data.push([day.getTime()]);
+        day.setDate(day.getDate() + 1);
+    }
+
+    var per_day = start_value / (week_days || delta_days);
+    var value = start_value;
+    for (var i = 0; i < data.length; i++) {
+        var day = new Date(data[i][0]);
+        data[i].push(value);
+        if (day.getDay() != 0 && day.getDay() != 6) {
+            value = value - per_day;
+        }
+    }
+    data.push([end.getTime(), 0]);
+
+     return {
+        data: data,
+        lines: {fill: true, lineWidth: 0},
+        points: {show: false},
+    };
+}
