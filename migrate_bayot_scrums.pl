a7b37912e18444066834d6b808bf27169c635fa0
Pami Ketolainen <pami.ketolainen@jollamobile.com>
2012-11-12 19:26:52 +0200

    Update to MPL2.0

--- migrate_bayot_scrums.pl
+++ migrate_bayot_scrums.pl
@@ -1,24 +1,12 @@
 #!/usr/bin/perl -w
 # -*- Mode: perl; indent-tabs-mode: nil -*-
 #
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-#
-# The Original Code is the AgileTools Bugzilla Extension.
-#
-# The Initial Developer of the Original Code is Pami Ketolainen
-# Portions created by the Initial Developer are Copyright (C) 2012 the
-# Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#   Pami Ketolainen <pami.ketolainen@gmail.com>
+# Copyright (C) 2012 Jolla Ltd.
+# Contact: Pami Ketolainen <pami.ketolainen@jollamobile.com>
 #
 #
 # Migration script to move team data from old BAYOT Scrums extension.
@@ -41,8 +29,6 @@ BEGIN { Bugzilla->extensions }
 use Bugzilla::Extension::AgileTools::Constants;
 use Bugzilla::Extension::AgileTools::Team;
 use Bugzilla::Extension::AgileTools::Role;
-
-use Data::Dumper;
 
 my $dbh = Bugzilla->dbh;
 
